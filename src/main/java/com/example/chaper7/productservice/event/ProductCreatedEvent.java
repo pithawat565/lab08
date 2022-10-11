068dca082cf4d35b21aa5eecf6c00d7a75a3ac47
LAB203_54 <pithawat2624@gmail.com>
2022-10-11 10:19:41 +0700

    LAB08

---
+++ src/main/java/com/example/chaper7/productservice/event/ProductCreatedEvent.java
@@ -0,0 +1,56 @@
+package com.example.chaper7.productservice.event;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class ProductCreatedEvent {
+
+    private String productId;
+    private String title;
+    private BigDecimal price;
+    private int quantity;
+
+    public ProductCreatedEvent() {
+    }
+
+    public ProductCreatedEvent(String productId, String title, BigDecimal price, int quantity) {
+        this.productId = productId;
+        this.title = title;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ src/main/java/com/example/chaper7/productservice/rest/CreateProductRestModel.java
@@ -0,0 +1,41 @@
+package com.example.chaper7.productservice.rest;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+@Data
+public class CreateProductRestModel{
+    private String title;
+    private BigDecimal price;
+    private Integer quantity;
+
+    public CreateProductRestModel(String title, BigDecimal price, int quantity) {
+        this.title = title;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
